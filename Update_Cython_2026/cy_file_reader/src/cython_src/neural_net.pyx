b451fbca12e2bbbaf5fb680b8e1f3710aceaa800
William Murphy <30298478+EveryTimeIWill18@users.noreply.github.com>
2026-08-11 12:01:39 -0400

    Update neural_net.pyx
    Added more methods.

--- Update_Cython_2026/cy_file_reader/src/cython_src/neural_net.pyx
+++ Update_Cython_2026/cy_file_reader/src/cython_src/neural_net.pyx
@@ -57,3 +57,68 @@ cdef class NeuralNetwork:
                 self.neural_net.layers[i].layer_nodes[j].successor_nodes = NULL
                 self.neural_net.layers[i].layer_nodes[j].weights = NULL
                 self.neural_net.layers[i].layer_nodes[j].num_successors = 0
+    
+    cpdef void set_layer_ids(self, list layer_ids):
+        """Set Layer ids"""
+        if self.neural_net.num_layers == 0 or self.neural_net.layers == NULL:
+            raise RuntimeError('Num_layers = 0 or neural_net.layers = NULL.')
+        cdef int i
+        cdef char* layer_id
+
+        for i in range(self.neural_net.num_layers):
+            layer_id = layer_ids[i]
+            self.neural_net.layers[i].layer_id = <char*>malloc((strlen(layer_id) + 1) * sizeof(char))
+            strcpy(self.neural_net.layers[i].layer_id, layer_id)
+
+    cpdef void set_node_ids(self, list node_ids):
+        """Set Node ids"""
+        if self.neural_net.num_layers == 0 or self.neural_net.layers == NULL:
+            raise RuntimeError('Num_layers = 0 or neural_net.layers = NULL.')
+
+        cdef int i, j
+        cdef int row
+        cdef char* node_id
+        row = 0
+
+        for i in range(self.neural_net.num_layers):
+            for j in range(self.neural_net.layers[i].num_nodes_in_layer):
+                node_id = node_ids[row]
+                print(f'{node_id=}')
+                self.neural_net.layers[i].layer_nodes[j].id = <char*>malloc((strlen(node_id)+1)* sizeof(char))
+                strcpy(self.neural_net.layers[i].layer_nodes[j].id, node_id)
+                row += 1
+
+    cpdef void initialize_weights(self):
+        """Initialize the weights"""
+        if self.neural_net.num_layers == 0 or self.neural_net.layers == NULL:
+            raise RuntimeError('Num_layers = 0 or neural_net.layers = NULL.')
+
+    cpdef dict get_network_structure(self):
+        """Returns the network structure in a dict"""
+        if self.neural_net == NULL or self.neural_net.layers == NULL:
+            return {}
+
+        cdef dict network_structure = {}
+        cdef int l, n
+        cdef Layer current_layer
+        cdef list node_ids
+        cdef str layer_id, node_id
+
+        for l in range(self.neural_net.num_layers):
+            current_layer = self.neural_net.layers[l]
+
+            if current_layer.layer_id != NULL:
+                layer_id = current_layer.layer_id.decode('utf-8')
+            else:
+                layer_id = f'L{l}'
+            node_ids = []
+
+            if current_layer.layer_nodes != NULL:
+                for n in range(current_layer.num_nodes_in_layer):
+                    if current_layer.layer_nodes[n].id != NULL:
+                        node_id = current_layer.layer_nodes[n].id.decode('utf-8')
+                    else:
+                        node_id = f'N{l}_{n}'
+                    node_ids.append(node_id)
+            network_structure[layer_id] = node_ids
+        return network_structure
